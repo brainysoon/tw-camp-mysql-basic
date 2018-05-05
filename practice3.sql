ac3257a72be5e526fd92d6360814d54ba7c0440c
brainysoon <brainysoon@gmail.com>
2018-05-05 23:26:59 +0800

    [ practice3 ] - Add practice3 sql to practice3.sql

--- practice3.sql
+++ practice3.sql
@@ -1,4 +1,4 @@
 #取得平均薪水最高的公司
 #输出公司名称和平均薪水：companyName avgSalary
 
-
+select c.companyName, e.avgSalary from (select avgT.companyId, avgT.avgSalary from (select companyId, avg(salary) as avgSalary from Employee group by companyId) avgT order by avgT.avgSalary limit 1) e left join Company c on e.companyId=c.id;
