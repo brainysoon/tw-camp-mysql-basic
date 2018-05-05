f49ae044fe9e7396346187371c70536beab2a5a4
brainysoon <brainysoon@gmail.com>
2018-05-05 23:35:56 +0800

    [ practice4 ] - Add practice4 sql to practice4.sql

--- practice4.sql
+++ practice4.sql
@@ -1,3 +1,5 @@
 # 找出薪水在公司的平均薪水之上的人员名字
 # 输出标准中包含Employee中所有字段和该employee所在公司名称以及该公司平均薪资：
 # id | name | age | gender | companyId | salary | companyName | avgsal
+
+select e.id,e.name,e.age,e.gender,e.companyId,e.salary, c.companyName, e.avgsal from (select *, avg(salary) as avgsal from Employee group by companyId having salary> avg(salary)) e left join Company c on c.id=e.companyId;
